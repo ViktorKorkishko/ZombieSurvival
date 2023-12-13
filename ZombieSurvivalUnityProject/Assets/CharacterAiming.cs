311506d4c98a29c32103bef28f76155209a602e1
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2023-12-13 18:00:58 +0200

    added weapon aiming

--- ZombieSurvivalUnityProject/Assets/CharacterAiming.cs
+++ ZombieSurvivalUnityProject/Assets/CharacterAiming.cs
@@ -1,21 +1,26 @@
-
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterAiming : MonoBehaviour
 {
     [SerializeField] private float _turnSpeed;
     [SerializeField] private Camera _camera;
 
-    [SerializeField] private CursorLocker _cursorLocker;
+    [SerializeField] private Rig aimRig;
+    [SerializeField] private float aimDuration;
 
     private void Update()
     {
-        if (!_cursorLocker.Locked)
-        {
-            return;
-        }
-
         float yawCamera = _camera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), _turnSpeed * Time.deltaTime);
+
+        if (Input.GetMouseButton(1))
+        {
+            aimRig.weight += Time.deltaTime / aimDuration;
+        }
+        else
+        {
+            aimRig.weight -= Time.deltaTime / aimDuration;
+        }
     }
 }
