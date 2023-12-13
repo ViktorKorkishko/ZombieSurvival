d1728fcd8ee3cb2968907d06bce698cf962f9399
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2023-12-13 22:53:00 +0200

    [wip] shooting

--- ZombieSurvivalUnityProject/Assets/Scripts/CharacterAiming.cs
+++ ZombieSurvivalUnityProject/Assets/Scripts/CharacterAiming.cs
@@ -6,21 +6,32 @@ public class CharacterAiming : MonoBehaviour
     [SerializeField] private float _turnSpeed;
     [SerializeField] private Camera _camera;
 
-    [SerializeField] private Rig aimRig;
-    [SerializeField] private float aimDuration;
+    [SerializeField] private Rig _aimRig;
+    [SerializeField] private float _aimDuration;
+
+    [SerializeField] private RaycastWeapon _raycastWeapon;
 
     private void Update()
     {
         float yawCamera = _camera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), _turnSpeed * Time.deltaTime);
 
-        if (Input.GetMouseButton(1))
+        if (Input.GetButton("Fire2"))
         {
-            aimRig.weight += Time.deltaTime / aimDuration;
+            _aimRig.weight += Time.deltaTime / _aimDuration;
         }
         else
         {
-            aimRig.weight -= Time.deltaTime / aimDuration;
+            _aimRig.weight -= Time.deltaTime / _aimDuration;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            _raycastWeapon.StartFiring();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            _raycastWeapon.StopFiring();
         }
     }
 }
--- ZombieSurvivalUnityProject/Assets/Scripts/RaycastWeapon.cs
+++ ZombieSurvivalUnityProject/Assets/Scripts/RaycastWeapon.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RaycastWeapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool _isFiring;
+    [SerializeField] private ParticleSystem _muzzleFlash;
+    [SerializeField] private ParticleSystem _hitEffect;
+    
+    [SerializeField] private Transform _raycastOrigin;
+    [SerializeField] private Transform _raycastDestination;
+
+    private Ray ray;
+    private RaycastHit raycastHit;
+
+    public void StartFiring()
     {
-        
+        _isFiring = true;
+        _muzzleFlash.gameObject.SetActive(true);
+
+        ray.origin = _raycastOrigin.position;
+        ray.direction = (_raycastDestination.position - _raycastOrigin.position).normalized;
+
+        if (Physics.Raycast(ray, out raycastHit))
+        {
+            Debug.DrawLine(ray.origin, raycastHit.point, Color.red, 1f);
+            _hitEffect.transform.position = raycastHit.point;
+            _hitEffect.transform.forward = raycastHit.normal;
+            _hitEffect.Emit(1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StopFiring()
     {
-        
+        _isFiring = false;
+        _muzzleFlash.gameObject.SetActive(false);
     }
 }
