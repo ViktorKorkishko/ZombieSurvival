5b139a6f3545ff73de826999c77cca38ac71b9b9
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2023-12-22 22:22:51 +0200

    [wip] WeaponInteractableObject

---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/InteractableObjects/Implementations/Weapon/Installers/InteractableWeaponInstaller.cs
@@ -0,0 +1,7 @@
+﻿using Game.InteractableObjects.Common.Installers;
+using Game.InteractableObjects.Implementations.Weapon.Controllers;
+
+namespace Game.InteractableObjects.Implementations.Weapon.Installers
+{
+    public class InteractableWeaponInstaller : InteractableObjectInstallerBase<InteractableWeaponController> { }
+}
