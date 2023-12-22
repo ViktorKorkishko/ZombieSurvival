257238e61b621d1ff72d59fd0719442434f775a5
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2023-12-22 22:20:29 +0200

    added CharacterObjectInteraction module

--- ZombieSurvivalUnityProject/Assets/Scripts/Game/Character/Weapons/PickUp/Models/WeaponPickUpModel.cs
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/Character/Weapons/PickUp/Models/WeaponPickUpModel.cs
@@ -1,15 +1,10 @@
 using System;
-using UnityEngine;
 using Zenject;
 
 namespace Game.Character.Weapons.PickUp.Models
 {
-    public class WeaponPickUpModel : MonoBehaviour
+    public class WeaponPickUpModel
     {
-        [SerializeField] private float _maxPickUpDistance;
-        
-        public float MaxPickUpDistance => _maxPickUpDistance;
-        
         public Action<DiContainer> OnWeaponPickedUp { get; set; }
 
         public void PickUp(DiContainer weaponContainer)
