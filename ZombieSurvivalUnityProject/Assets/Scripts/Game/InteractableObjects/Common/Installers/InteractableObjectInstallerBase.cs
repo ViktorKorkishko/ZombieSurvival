5a2f8a92f0a01224a00eb99135a22641a0a858e1
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2023-12-22 22:21:34 +0200

    added base logic for interactable objects

---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/InteractableObjects/Common/Installers/InteractableObjectInstallerBase.cs
@@ -0,0 +1,26 @@
+﻿using Game.InteractableObjects.Common.Enums;
+using Game.InteractableObjects.Common.Models;
+using Game.InteractableObjects.Common.Views;
+using UnityEngine;
+using Zenject;
+
+namespace Game.InteractableObjects.Common.Installers
+{
+    public abstract class InteractableObjectInstallerBase<T> : MonoInstaller
+    {
+        [Header("Components")]
+        [SerializeField] private InteractableObjectView _interactableObjectView;
+        
+        [Header("Object type")]
+        [SerializeField] private InteractableObjectType _type;
+        
+        public override  void InstallBindings()
+        {
+            Container.Bind<InteractableObjectModel>().AsSingle();
+            Container.BindInstance(_interactableObjectView).AsSingle();
+            Container.BindInterfacesAndSelfTo<T>().AsSingle();
+            
+            Container.BindInstance(_type).AsSingle();
+        }
+    }
+}
---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/InteractableObjects/Common/Views/InteractableObjectView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Game.InteractableObjects.Common.Views
+{
+    public class InteractableObjectView : MonoBehaviour
+    {
+        [SerializeField] private Context _context;
+
+        public Context Context => _context;
+    }
+}
