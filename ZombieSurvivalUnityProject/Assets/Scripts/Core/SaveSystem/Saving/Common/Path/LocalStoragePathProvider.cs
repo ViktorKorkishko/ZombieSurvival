c54a0c140ae40f213e3c8c178f9d4b3b27d943bf
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2024-01-30 19:25:07 +0200

    added LocalStoragePathProvider and EditorHelper

---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Core/SaveSystem/Saving/Common/Path/LocalStoragePathProvider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Core.SaveSystem.Saving.Common.Path
+{
+    public class LocalStoragePathProvider
+    {
+        public string Path => Application.persistentDataPath;
+    }
+}
---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Core/SaveSystem/Saving/Editor/LocalStorageEditorHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Core.SaveSystem.Saving.Common.Path;
+using UnityEditor;
+
+namespace Core.SaveSystem.Saving.Editor
+{
+    public class LocalStorageEditorHelper
+    {
+#if UNITY_EDITOR
+        [MenuItem("SaveSystem/Open local folder")]
+        public static void OpenSaveFolder()
+        {
+            var localPathProvider = new LocalStoragePathProvider();
+            Process.Start(localPathProvider.Path);
+        }
+#endif
+    }
+}
