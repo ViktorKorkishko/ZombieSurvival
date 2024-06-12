060e47d68b6e268bf4e47ffb5e51debbb679a644
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2024-06-12 21:28:29 +0300

    added Health stat

---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/Stats/Core/BaseStat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Game.Stats.Core
+{
+    public abstract class BaseStat<T>
+    {
+        public T CurrentValue { get; protected set; }
+        public T MinValue { get; }
+        public T MaxValue { get; }
+
+        public event Action<T> OnValueChanged;
+
+        public BaseStat(T initialCurrentValue,T minValue, T maxValue)
+        {
+            CurrentValue = initialCurrentValue;
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+        
+        public abstract void AdjustValue(T value);
+        
+        protected void InvokeValueChanged(T value)
+        {
+            OnValueChanged?.Invoke(value);
+        }
+    }
+}
---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/Stats/Health/HealthStat.cs
@@ -0,0 +1,19 @@
+﻿using Game.Stats.Core;
+
+namespace Game.Stats.Health
+{
+    public class HealthStat : BaseStat<float>
+    {
+        public override void AdjustValue(float value)
+        {
+            CurrentValue += value;
+            InvokeValueChanged(value);
+        }
+
+        public HealthStat(float initialCurrentValue, float minValue, float maxValue)
+            : base(initialCurrentValue, minValue, maxValue)
+        {
+            
+        }
+    }
+}
