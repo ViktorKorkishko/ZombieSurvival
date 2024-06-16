d68306a9290f93d4cba17080ea7071e03230ac95
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2024-06-17 00:19:25 +0300

    added health inspector drawer

--- ZombieSurvivalUnityProject/Assets/Scripts/Game/Stats/Health/Models/HealthModel.Editor.cs
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/Stats/Health/Models/HealthModel.Editor.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using Sirenix.OdinInspector;
+#endif
+using UnityEngine;
 
 namespace Game.Stats.Health.Models
 {
     public partial class HealthModel
     {
 #if UNITY_EDITOR
+        [ShowInInspector] 
+        private float CurrentHealth => HealthStat?.CurrentValue ?? 0f;
+        
         [ContextMenu(nameof(Deal20))]
         public void Deal20()
         {
