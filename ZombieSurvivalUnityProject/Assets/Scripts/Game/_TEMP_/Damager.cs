085c7e30b02489254d90a35cba3a92d9574e8868
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2024-06-17 00:28:55 +0300

    fixed temp folders

---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/_TEMP_/Damager.cs
@@ -0,0 +1,19 @@
+using Game.Stats.Health.View;
+using UnityEngine;
+
+namespace Game.TEMP
+{
+    public class Damager : MonoBehaviour
+    {
+        [Range(-5, 5)] [SerializeField] private float _damage;
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.TryGetComponent<HitboxView>(out var hitboxView))
+            {
+                var adjustValue = -_damage * Time.deltaTime;
+                hitboxView.HealthModel.AdjustHealth(adjustValue);
+            }
+        }
+    }
+}
