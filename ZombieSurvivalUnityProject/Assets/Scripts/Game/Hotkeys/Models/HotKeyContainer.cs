414d03394e31382f6aa16fe3bd75a748b0424299
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2024-07-04 00:35:01 +0300

    added HotKeys core module

---
+++ ZombieSurvivalUnityProject/Assets/Scripts/Game/Hotkeys/Models/HotKeyContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game.Hotkeys.Models
+{
+    public class HotKeyContainer
+    {
+        public Action Action { get; }
+        public object Source { get; }
+        
+        public HotKeyContainer(Action action, object source)
+        {
+            Action = action;
+            Source = source;
+        }
+    }
+}
